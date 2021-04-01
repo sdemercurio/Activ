a516475a00025fbd0d75af9e7591314660843b4b
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-01 14:39:49 -0400

    creating server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.use(express.static("/public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+app.use(require("./routes/html.js"));
+app.use(require("./routes/api"));
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  app.listen(PORT, () =>
+    console.log(`App listening on http://localhost:${PORT}`)
+  );
+});
