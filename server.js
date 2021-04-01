a91165ebbd1d3e9f12fcc49e84d2cf0744340f1d
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-01 14:43:37 -0400

    creating routes folder and files

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false,
 });
 
-app.use(require("./routes/html.js"));
-app.use(require("./routes/api"));
+app.use(require("./routes/htmlRoutes.js"));
+app.use(require("./routes/apiRoutes"));
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
