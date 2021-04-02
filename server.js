c94d24dae847f4e092d06b2763eb34d802227f84
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-02 16:28:30 -0400

    fixing deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/aktiv", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
@@ -20,6 +20,12 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/aktiv", {
 
 // routes
 app.use(require("./routes/apiRoutes.js"));
+
+mongoose.connection
+  .once("open", () => console.log("connected"))
+  .on("error", (error) => {
+  console.log("error connecting", error);
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
