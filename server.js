1352fcdd2aba1716e95e4a3e740eea87935a4845
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-02 13:23:54 -0400

    deploying

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/aktiv',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/aktiv", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/apiRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
