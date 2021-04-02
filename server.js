84e4633c42ef4e98974a05155be2bc667d2a8f9b
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-02 13:01:37 -0400

    deploying

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("/public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Aktiv", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
