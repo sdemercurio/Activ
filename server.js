84a28eef3dd51f7080f4080eb6358e3538219184
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-02 13:04:34 -0400

    deploying

--- server.js
+++ server.js
@@ -6,24 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(express.json());
-
-app.use(express.static("/public"));
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Aktiv", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
-
-app.use(require("./routes/htmlRoutes"));
-app.use(require("./routes/apiRoutes"));
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
