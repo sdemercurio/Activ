7a92e4a65ebdd0cde45f45c32adec28caa566ac9
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-03 16:24:19 -0400

    fixing heroku issues

--- server.js
+++ server.js
@@ -11,15 +11,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+
 
 // routes
 app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
