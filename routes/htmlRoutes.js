98cb787bd54c6d53257b4c964f2c4c8adf89a7b6
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-03 20:26:13 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,7 +6,7 @@ const path = require("path");
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
-display stats.html
+// display stats.html
 router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
